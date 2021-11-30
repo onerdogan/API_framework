3d3e5480b29989e144fa2e3e19c01694f0bab52b
onerdogan <onerdogan91@gmail.com>
2021-12-01 00:34:29 +0300

    a8

---
+++ src/main/java/com/techproed/day14/ReqresRequest.java
@@ -0,0 +1,14 @@
+package com.techproed.day14;
+
+import org.junit.Test;
+
+public class ReqresRequest {
+
+    @Test
+    public void test(){
+        ReqresToken obje=new ReqresToken();
+        System.out.println(obje.tokenAl());
+
+
+    }
+}
